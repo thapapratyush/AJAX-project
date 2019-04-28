52c58ec3850f91cca3f0e96d294e2da72708f258
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-27 21:40:56 -0400

    Successfully log all the category types

--- booklist.php
+++ booklist.php
@@ -12,9 +12,6 @@ $dbase = connecttodb();
 if($dbase != NULL){
     $categories = mysqli_query($dbase, "select * from category");
     $list_of_all_categories = array();
-    for ($i=0; $i<count($categories->fetch_assoc());$i++){
-         array_push($list_of_all_categories , $categories->fetch_assoc()[category]);
-    }
-    echo $list_of_all_categories;
+    echo json_encode($categories->fetch_all());
 }
 ?>
