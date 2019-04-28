1403edc1727e2896a5c152210f1ad9dc0f0ec1f2
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-28 19:01:09 -0400

    Add query for category and echo for now

--- booklist.php
+++ booklist.php
@@ -18,12 +18,28 @@ function printXMLorJSON($printArray, $parameter){
     }
 }
 
+function querydbforBooksfromCategories($chosenCategory){
+    foreach($chosenCategory as $category){
+        $query = "select t.title_name, a.author, y.year, c.category from title t ";
+        $query .= "join category c on c.category_id = t.category_id and ";
+        $query .= "c.category_id=" . $category . " ";
+        $query .= "join year y on y.title_id = t.title_id ";
+        $query .= "join author a on a.author_id = t.author_id;";
+
+        echo $query;
+        $booksofcategory = mysqli_query($dbase, $query);
+
+        // echo json_encode($booksofcategory);
+    }
+        
+}
+
 $dbase = connecttodb();
 $parameter = $_GET["format"];
-$selectedCategory = $_GET["selectedCategory"];
 
 
 if($dbase != NULL){
+    $selectedCategory = $_GET["selectedCategory"];
     if($selectedCategory=="false") {
         $categories = mysqli_query($dbase, "select * from category");
         $list_of_all_categories = array();
@@ -33,7 +49,7 @@ if($dbase != NULL){
         }
         printXMLorJSON($list_of_all_categories, $parameter);
     }else{
-        echo $selectedCategory;
+        querydbforBooksfromCategories(explode(",",$selectedCategory));
     }
 }
 ?>
